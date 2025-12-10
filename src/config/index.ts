bc9bed5d8200a100ec12014d6ca4aa07518df163
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-10 19:52:35 +0600

    login controller done

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,8 @@ dotenv.config({
 const config = {
     port: Number(process.env.PORT),
     connectionStr: process.env.CONNECTION_STR,
-    bcryptCircleCount: Number(process.env.BCRYPT_CIRCLE_COUNT)
+    bcryptCircleCount: Number(process.env.BCRYPT_CIRCLE_COUNT),
+    jwtSecret: process.env.JWT_SECRET as string
 }
 
 export default config;
--- src/modules/auth.servere.ts
+++ src/modules/auth.servere.ts
@@ -1,6 +1,8 @@
 import config from "../config";
 import { pool } from "../config/db";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import AppError from "../utils/AppError";
 
 const authServices: Record<string, unknown> = {};
 
@@ -16,9 +18,30 @@ authServices.register = async (userData: UserData) => {
     const { name, email, password, phone, role } = userData;
     const hashedPass = await bcrypt.hash(password, config.bcryptCircleCount);
     const result = await pool.query(`
-        INSERT INTO users (name, email, password, phone, role) RETURNING *
+        INSERT INTO users (name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *
         `, [name, email, hashedPass, phone, role]);
     return result.rows[0];
+}
+
+authServices.login = async (emailAddr: string, password: string) => {
+    const result = await pool.query(`SELECT * FROM users WHERE email=$1`, [emailAddr]);
+    const user = result.rows[0];
+    if (!user) {
+        throw new AppError(500, "User not found!");
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+        throw new AppError(500, "Invalid Password!");
+    }
+    const { id, email, role } = user;
+    const token = jwt.sign({ id, email, role }, config.jwtSecret, {
+        expiresIn: "30d"
+    });
+
+    return {
+        user,
+        token
+    }
 }
 
 export default authServices;
