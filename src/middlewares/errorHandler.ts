fe14939070ebe519adf664b44a28c83b4a465f88
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-10 18:42:08 +0600

    custom Error Handler setup done

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import AppError from "../utils/AppError";
+import sendResponse from "../utils/sendResponse";
 
-const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json()
+const errorHandler = (error: AppError | Error, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = error instanceof AppError ? error.statusCode : 500;
+    const message = error.message || "Internal Server Error!";
+    sendResponse(res, statusCode, {
+        success: false,
+        message,
+        error
+    });
 }
+
+export default errorHandler;
