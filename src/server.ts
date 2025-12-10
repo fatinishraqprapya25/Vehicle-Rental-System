5d37dc9045fd3a1f1bb3948714c7450b806f3e6c
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-10 18:43:30 +0600

    server page optimized

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import config from "./config";
 import router from "./routes";
+import notFoundHandler from "./middlewares/notFoundHandler";
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Server listening at port ${config.port}`);
