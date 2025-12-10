d41a0808c6a740926a8a9629effdeb9e84ffacfd
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-10 20:25:24 +0600

    login controllers done

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,10 +4,10 @@ import authServices from "./auth.servere";
 
 const authControllers: any = {};
 
-authControllers.register = (req: Request, res: Response) => {
+authControllers.register = async (req: Request, res: Response) => {
     try {
         const userInfo = req.body;
-        const user = authServices.register(userInfo);
+        const user = await authServices.register(userInfo);
         if (!user) {
             return sendResponse(res, 500, {
                 success: false,
@@ -27,3 +27,31 @@ authControllers.register = (req: Request, res: Response) => {
         });
     }
 }
+
+authControllers.login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return sendResponse(res, 500, {
+                success: false,
+                message: "Please provide proper informations to sign in"
+            });
+        }
+        const user = await authServices.login(email, password);
+        const { token, data } = user;
+        sendResponse(res, 200, {
+            success: true,
+            message: "User logged in successfully!",
+            data,
+            token
+        });
+    } catch (error: any) {
+        sendResponse(res, 500, {
+            success: false,
+            message: error.message,
+            error
+        });
+    }
+}
+
+export default authControllers;
