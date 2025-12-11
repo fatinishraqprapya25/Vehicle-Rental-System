b65d004c6b69b39d18916e65d59ddc2190e3218d
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-12 01:21:34 +0600

    update user controller optimized

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -23,14 +23,24 @@ userControllers.getAllUsers = async (req: Request, res: Response) => {
 
 userControllers.updateUser = async (req: Request, res: Response) => {
     try {
+        const user = req.user!;
         const userId = req.params.id;
-        const userInfo = req.body;
-        const result = await usersServices.updateUser(userId, userInfo);
-        sendResponse(res, 200, {
-            success: true,
-            message: "User info updated successfully!",
-            data: result
+
+        if (user.role === "admin" || user.id === userId) {
+            const userInfo = req.body;
+            const result = await usersServices.updateUser(userId, userInfo);
+            return sendResponse(res, 200, {
+                success: true,
+                message: "User info updated successfully!",
+                data: result
+            });
+        }
+
+        sendResponse(res, 403, {
+            success: false,
+            message: "You don't have permission to do the operation!"
         });
+
     } catch (error: any) {
         sendResponse(res, 500, {
             success: false,
