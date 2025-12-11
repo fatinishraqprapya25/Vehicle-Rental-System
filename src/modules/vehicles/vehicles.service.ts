a5e04bcb09417faebb055fee03d31301f3eca24e
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-11 12:08:29 +0600

    update vehicle service done

--- src/modules/vehicles/vehicles.service.ts
+++ src/modules/vehicles/vehicles.service.ts
@@ -10,6 +10,8 @@ interface Vehicle {
     availability_status: "available" | "booked";
 }
 
+type OptionalVehicle = Partial<Vehicle>;
+
 vehicleServices.createVehicle = async (vehicleData: Vehicle) => {
     const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = vehicleData;
     const result = await pool.query(`
@@ -26,6 +28,18 @@ vehicleServices.getAllVehicles = async () => {
 
 vehicleServices.geVehicleById = async (vehicleId: string) => {
     const result = await pool.query("SELECT * FROM vehicles WHERE id=$1", [vehicleId]);
+    return result.rows[0];
+}
+
+vehicleServices.updateVehicle = async (vehicleId: string, vehicleData: OptionalVehicle) => {
+    const keys = Object.keys(vehicleData);
+    if (keys.length === 0) {
+        return null;
+    }
+    const setQuery = keys.map((key: string, index: number) => `${key}=$${index + 1}`).join(", ");
+    const values = Object.values(vehicleData);
+    const query = `UPDATE vehicles SET ${setQuery} WHERE id=$${keys.length + 1} RETURNING *`;
+    const result = await pool.query(query, [...values, vehicleId]);
     return result.rows[0];
 }
 
