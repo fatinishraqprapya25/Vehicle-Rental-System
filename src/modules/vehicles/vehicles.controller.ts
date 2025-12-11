6afeeaf06b8dff5a039bfdbc7e397fb84dea5aa0
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-11 12:16:10 +0600

    create vehicle controller done

---
+++ src/modules/vehicles/vehicles.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import sendResponse from "../../utils/sendResponse";
+import vehicleServices from "./vehicles.service";
+
+const vehicleControllers: any = {};
+
+vehicleControllers.createVehicle = async (req: Request, res: Response) => {
+    try {
+        const vehicleDetails = req.body;
+        const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = vehicleDetails;
+        if (!vehicle_name || !type || !registration_number || !daily_rent_price || !availability_status) {
+            return sendResponse(res, 400, {
+                success: false,
+                message: "Please provide proper informations to create vehicle!"
+            });
+        }
+        const result = await vehicleServices.createVehicle(vehicleDetails);
+        sendResponse(res, 201, {
+            success: true,
+            message: "Vehicle created successfully!",
+            data: result
+        });
+    } catch (error: any) {
+        sendResponse(res, 500, {
+            success: false,
+            message: error.message,
+            error
+        });
+    }
+}
+
+
+
+export default vehicleControllers;
