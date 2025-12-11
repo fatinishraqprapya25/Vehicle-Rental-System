73f20f5935bbf1579f54e8a63a40eb742e06e335
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-12 00:54:58 +0600

    users service optimized

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -7,9 +7,4 @@ usersServices.getAllUsers = async () => {
     return result;
 }
 
-usersServices.getUserById = async (userId: string) => {
-    const result = await pool.query(`SELECT * FROM users WHERE id=$1`, [userId]);
-    return result;
-}
-
 export default usersServices;
