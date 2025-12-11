9a5d747910aac430f08b0bf8c99d758b8e8a29d8
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-12 01:30:09 +0600

    routing setup for user done

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import authRouter from "../modules/auth/auth.routes";
 import vehicleRouter from "../modules/vehicles/vehicles.routes";
+import userRouter from "../modules/users/users.routes";
 
 const router = express.Router();
 
 const routes = [
     { path: "/auth", handler: authRouter },
-    { path: "/vehicles", handler: vehicleRouter }
+    { path: "/vehicles", handler: vehicleRouter },
+    { path: "/users", handler: userRouter }
 ];
 
 routes.forEach(r => router.use(r.path, r.handler));
