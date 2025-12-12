c75995314cbb65ebd7d919b8ce1a92c3837c67ee
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-12 15:01:40 +0600

    basic booking controller template setup done

---
+++ src/modules/bookings/booking.controller.ts
@@ -0,0 +1,9 @@
+import { Request } from "express";
+
+const bookingControllers: any = {};
+
+bookingControllers.createBooking = async (req: Request, res: Response) => {
+
+}
+
+export default bookingControllers;
