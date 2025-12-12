a5a8633d7e1bb00ca03426ab9cd29b7f4cc154cc
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-12 14:21:09 +0600

    create booking service setup done

--- src/modules/bookings/booking.service.ts
+++ src/modules/bookings/booking.service.ts
@@ -1,1 +1,24 @@
+import { pool } from "../../config/db";
+
 const bookingServices: any = {};
+
+interface Booking {
+    id: string;
+    customer_id: string;
+    vehicle_id: string;
+    rent_start_at: Date;
+    rent_end_at: Date;
+    total_price: number;
+    status: "active" | "booked" | "returned";
+}
+
+bookingServices.createBooking = async (bookingData: Booking) => {
+    const { customer_id, vehicle_id, rent_start_at, rent_end_at, total_price, status } = bookingData;
+    const result = await pool.query(`
+        INSERT INTO bookings(customer_id, vehicle_id, rent_start_at, rent_end_at, total_price, status) VALUES($1, $2, $3, $4, $5, $6)`,
+        [customer_id, vehicle_id, rent_start_at, rent_end_at, total_price, status]
+    );
+    return result;
+}
+
+export default bookingServices;
