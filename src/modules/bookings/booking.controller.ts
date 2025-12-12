74a67edae9440573087eb4f720966eeac331592a
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-12-12 15:29:08 +0600

    create booking controller optimized again

--- src/modules/bookings/booking.controller.ts
+++ src/modules/bookings/booking.controller.ts
@@ -1,13 +1,26 @@
 import { Request, Response } from "express";
 import sendResponse from "../../utils/sendResponse";
+import bookingServices from "./booking.service";
 
 const bookingControllers: any = {};
 
 bookingControllers.createBooking = async (req: Request, res: Response) => {
     try {
         const bookingData = req.body;
-        const userId = req.user!.id;
-        const { customer_id, vehicle_id, rent_start_at, rent_end_at } = bookingData;
+        const customer_id = req.user!.id;
+        const { vehicle_id, rent_start_at, rent_end_at } = bookingData;
+        if (!customer_id || !vehicle_id || !rent_start_at || !rent_end_at) {
+            return sendResponse(res, 400, {
+                success: false,
+                message: "Please provide proper informations to book vehicle!"
+            });
+        }
+        const result = await bookingServices.createBooking(bookingData);
+        sendResponse(res, 201, {
+            success: true,
+            message: "Booking created successfully!",
+            data: result
+        });
     } catch (error: any) {
         sendResponse(res, 500, {
             success: false,
